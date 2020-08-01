6d76959d932efdcd853c79f2fa77b9710e13e0e8
htstuck <htstuck@gmail.com>
2020-07-31 20:39:52 -0700

    added total portfolio information

--- stock_info.py
+++ stock_info.py
@@ -1,64 +1,75 @@
 import finnhub
-import json
 import smtplib
 import email
 import sys
+import os 
 
 def main():
     # Setup client
-    with open('api_keys.json') as f1:
-        FINNHUB_KEY = json.load(f1)['finnhub']
-    finnhub_client = finnhub.Client(api_key=FINNHUB_KEY)
+    PRIVATE_API_KEY = os.environ.get('PRIVATE_API_KEY')
+    finnhub_client = finnhub.Client(api_key=PRIVATE_API_KEY)
 
     # Load portfolio
     with open(sys.argv[1]) as f2:
         portfolio = f2.readlines()
 
-    # portfolio_value = 0.0
-    # todays_return = 0.0
-    # movers = []
-
+    total_portfolio_val, total_return_today, total_close_yesterday = 0.0, 0.0, 0.0
+    big_movers = []
+    stocks = []
     for stock in portfolio:
+        # Get components of stock information
         ticker, shares, price = tuple(stock.split(','))
+        shares = int(shares)
+        buy_price = float(price)
+        # Get the quote for the given ticker
         quote = finnhub_client.quote(ticker)
-        get_stock_info(quote, ticker, int(shares), float(price))
+        # Important stats
+        close_yesterday = quote['pc']
+        close_today = quote['c']
+        open_today = quote['o']
+        high_today = quote['h']
+        low_today = quote['l']
+        change = close_today - close_yesterday
+        percent_change = change / close_yesterday * 100
 
+        # Collect overall portolfio data
+        total_portfolio_val += close_today * shares
+        total_return_today +=  change * shares
+        total_close_yesterday += close_yesterday
 
-# def get_porfolio_info(quote, ticker, shares, buy_price):
-    # pass
+        # Notify user with '*' if stock goes up/down by more than 5%
+        if abs(percent_change) >= 5.0:
+            ticker = "*  " + ticker 
+            big_movers.append(ticker)
+        
+        # Information as list of strings for easier formatting
+        stock = ['\n', ticker, '~'*35]
+        # Today's return = (close price today - close price yesterday) * num of shares
+        stock.append('Today\'s Return: {}  ({})'.format(as_currency(change * shares), as_percent(percent_change)))
+        # Total return = (close today - buy price) * num of shares
+        stock.append('Total Return: {}  ({})'.format(as_currency((close_today - buy_price) * shares), as_percent((close_today - buy_price) / buy_price * 100)))
+        stock.append('-'*35)
+        # Market value = close price * num of shares
+        stock.append('Market Value ${:.2f}'.format(close_today * shares))
+        # Number of shares
+        stock.append('# of Shares: {}'.format(shares))
 
-def get_stock_info(quote, ticker, shares, buy_price):
-    """Returns the daily information for a specific ticker."""
-    # Get the quote for the given ticker
-    close_yesterday = quote['pc']
-    close_today = quote['c']
-    open_today = quote['o']
-    high_today = quote['h']
-    low_today = quote['l']
-    change = close_today - close_yesterday
-    percent_change = change / close_yesterday * 100
+        stock.append('-'*35)
+        # Ticker open, close, high, and low for the day
+        stock.append('Open: ${:.2f}   |   High: {:.2f}'.format(open_today, high_today))
+        stock.append('Close: ${:.2f}  |   Low: {:.2f}'.format(close_today, low_today))
 
-    # Notify user with '*' if stock goes up/down by more than 5%
-    if abs(percent_change) >= 5.0:
-        ticker = "*  " + ticker 
-    
-    # Information as list of strings for easier formatting
-    stock = ['\n', ticker, '~'*35]
-    # Market value = close price * num of shares
-    stock.append('Market Value ${:.2f}'.format(close_today * shares))
-    # Number of shares
-    stock.append('# of Shares: {}'.format(shares))
-    # Today's return = (close price today - close price yesterday) * num of shares
-    stock.append('Today\'s Return: {}  ({})'.format(as_currency(change * shares), as_percent(percent_change)))
-    # Total return = (close today - buy price) * num of shares
-    stock.append('Total Return: {}  ({})'.format(as_currency((close_today - buy_price) * shares), as_percent((close_today - buy_price) / buy_price * 100)))
-    stock.append('-'*35)
-    # Ticker open, close, high, and low for the day
-    stock.append('Open: ${:.2f}   |   High: {:.2f}'.format(open_today, high_today))
-    stock.append('Close: ${:.2f}  |   Low: {:.2f}'.format(close_today, low_today))
-
-    # Print all data
-    print('\n'.join(stock))
+        stocks.append(stock)
+    # Print overall data
+    print('~'*35)
+    print('YOUR PORTFOLIO')
+    print('~'*35)
+    print('Total Portolfio Value: ${:.2f}'.format(total_portfolio_val))
+    print('Today\'s Return: {} ({})'.format(as_currency(total_return_today), as_percent(total_return_today / total_close_yesterday * 100)))
+    print('Big Movers:', ', '.join(big_movers))
+    # Print indv. stock data
+    for stock in stocks:
+        print('\n'.join(stocks))
 
 
 def as_currency(amount):
@@ -72,10 +83,7 @@ def as_percent(percent):
     if percent >= 0:
         return '+{:.2f}%'.format(percent)
     else:
-        return '-${:.2f}%'.format(percent)
-
-
-
+        return '-{:.2f}%'.format(abs(percent))
 
 
 if __name__ == "__main__":
